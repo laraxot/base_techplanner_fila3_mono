981e9285bdfacea51537981df5799118ef945eeb
marco76tv <marco.sottana@gmail.com>
2025-09-01 14:37:04 +0000

    .

--- laravel/Modules/Xot/app/Filament/Resources/RelationManagers/XotBaseRelationManager.php
+++ laravel/Modules/Xot/app/Filament/Resources/RelationManagers/XotBaseRelationManager.php
@@ -7,10 +7,8 @@ namespace Modules\Xot\Filament\Resources\RelationManagers;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager as FilamentRelationManager;
 use Filament\Tables;
-use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 use Modules\Xot\Filament\Traits\HasXotTable;
 use Webmozart\Assert\Assert;
@@ -47,49 +45,93 @@ abstract class XotBaseRelationManager extends FilamentRelationManager
     {
         return $this->getResource()::getFormSchema();
     }
-//*
+
+    // *
     public function getTableColumns(): array
     {
-        $index=Arr::get($this->getResource()::getPages(),'index');
-        if(!$index){
-            //throw new \Exception('Index page not found');
+        $index = Arr::get($this->getResource()::getPages(), 'index');
+        if (! $index) {
+            // throw new \Exception('Index page not found');
             return [];
         }
         /** @phpstan-ignore method.nonObject */
-        $index_page=$index->getPage();
-        
-        if(!method_exists($index_page,'getTableColumns')){
-            //throw new \Exception('method  getTableColumns on '.print_r($index_page,true).' not found');
+        $index_page = $index->getPage();
+
+        if (! method_exists($index_page, 'getTableColumns')) {
+            // throw new \Exception('method  getTableColumns on '.print_r($index_page,true).' not found');
             return [];
         }
         /** @phpstan-ignore argument.type */
-        $res= app($index_page)->getTableColumns();
+        $res = app($index_page)->getTableColumns();
 
         return $res;
     }
-//*/
+
+    // */
     public function getTableActions(): array
     {
-        return [
-            Tables\Actions\EditAction::make(),
-            //Tables\Actions\DeleteAction::make(),
-            Tables\Actions\DetachAction::make(),
-        ];
+
+
+        $actions = [];
+        $resource = $this->getResource();
+        
+         
+        if (method_exists($resource, 'canEdit')) {
+            $actions['edit'] = Tables\Actions\EditAction::make()
+                //->iconButton()
+                ->visible(fn (Model $record): bool => $resource::canEdit($record));
+        }
+
+        if (method_exists($resource, 'canDetach')) {
+            $actions['detach'] = Tables\Actions\DetachAction::make()
+                //->iconButton()
+                ->visible(fn (Model $record): bool => $resource::canDetach($record));
+        }
+        if (method_exists($resource, 'canDelete')) {
+            $actions['delete'] = Tables\Actions\DeleteAction::make()
+                //->iconButton()
+                ->visible(fn (Model $record): bool => $resource::canDelete($record));
+        }
+
+       
+        return $actions;
     }
 
     public function getTableBulkActions(): array
     {
         return [
-            //Tables\Actions\DeleteBulkAction::make(),
+            // Tables\Actions\DeleteBulkAction::make(),
             Tables\Actions\DetachBulkAction::make(),
         ];
     }
 
     public function getTableHeaderActions(): array
     {
+        /*
         return [
-            Tables\Actions\AttachAction::make(),
+            Tables\Actions\AttachAction::make()
+               ->icon('heroicon-o-link'),
         ];
+        */
+        $actions = [];
+        $resource = $this->getResource();
+        
+         
+        if (method_exists($resource, 'canAttach')) {
+            $actions['attach'] = Tables\Actions\AttachAction::make()
+                ->icon('heroicon-o-link')
+                //->iconButton()
+                ->visible(fn (?Model $record): bool => $resource::canAttach());
+        }
+
+        if (method_exists($resource, 'canCreate')) {
+            $actions['create'] = Tables\Actions\CreateAction::make()
+                //->iconButton()
+                ->visible(fn (?Model $record): bool => $resource::canCreate());
+        }
+
+       
+        return $actions;
     }
 
     public function getTableFilters(): array
@@ -99,7 +141,7 @@ abstract class XotBaseRelationManager extends FilamentRelationManager
 
     public function getResource(): string
     {
-        $resource = static::$resourceClass;
+        $resource = static::$resource;
         Assert::classExists($resource);
         Assert::isAOf($resource, XotBaseResource::class);
 
