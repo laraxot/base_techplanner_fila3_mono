41ffcb5bd0354a79a800b79265b3288d75bb555c
marco76tv <marco.sottana@gmail.com>
2025-09-10 22:04:13 +0200

    Modify test files in branch

--- test_conflicts/file3.js
+++ test_conflicts/file3.js
@@ -1,12 +1,8 @@
 // JavaScript con conflitti
 const config = {
     name: "test",
-<<<<<<< HEAD
-    version: "1.0.0",
-    debug: true
-=======
-    version: "2.0.0",
+    version: "3.0.0",
     debug: false,
-    newFeature: true
->>>>>>> develop
+    newFeature: true,
+    branchFeature: "enabled"
 };
